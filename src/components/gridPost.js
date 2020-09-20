2eef8caba80c8f8c30e015d475300c3e341dc1c1
anup-a <aglawe.anup22@gmail.com>
2020-09-20 12:56:54 +0530

    ⚜ added hover animation on grid posts

--- src/components/gridPost.js
+++ src/components/gridPost.js
@@ -7,21 +7,25 @@ function GridPost({ post, ceil }) {
     backgroundImage: `url("${require(`../assets/images/${post.image}`)}" )`,
   };
 
-  const postStyles =
-    windowWidth > 900 ? { ...bgStyle, ...post.style } : { ...bgStyle };
+  const postStyles = windowWidth > 900 ? { ...post.style } : {};
 
   const styles = ceil
     ? { ...postStyles, justifyContent: "space-between" }
     : { ...postStyles };
 
+  const containerStyle = ceil ? { justifyContent: "space-between" } : {};
+
   const postCategories = post.categories.split(" ");
 
   return (
     <div className="grid-post overlay" style={styles} href={post.link}>
-      <TagsContainer postCategories={postCategories} />
-      <div className="image-text">
-        <h3 className="image-title">{post.title}</h3>
-        <span className="image-date">{post.date}</span>
+      <div className="bg-blog-image" style={bgStyle}></div>
+      <div className="grid-post-container" style={containerStyle}>
+        <TagsContainer postCategories={postCategories} />
+        <div className="image-text">
+          <h3 className="image-title">{post.title}</h3>
+          <span className="image-date">{post.date}</span>
+        </div>
       </div>
     </div>
   );
