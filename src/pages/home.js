bac4186e1ab31f2826009b48da334fca4797275a
anup-a <aglawe.anup22@gmail.com>
2020-11-13 00:15:40 +0530

    fixed home styling issues

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Col, Row } from 'antd'
+import { Col, Divider, Row } from 'antd'
 
 import PostsGrid from '../components/grid/PostsGrid'
-import trending from './../assets/mocks/trending'
-import featured from './../assets/mocks/featured'
-import GridPost from '../components/grid/gridPost'
-import PostsList from '../components/post/postsList'
 import SideBar from '../components/sideBar'
+import PostsList from '../components/post/postsList'
+import { usePostsQuery } from '../graphql/queries'
+import { useLocation, useParams } from 'react-router'
+import { PAGE_SIZE } from '../config'
+import { divider } from 'Dante2/package/es/components/icons'
 // import { featuredGridConfig, mergeStyles, trendingGridConfig } from '../helpers/helpers'
 
 // const trendingPosts = mergeStyles(trending, trendingGridConfig)
@@ -15,6 +16,40 @@ import SideBar from '../components/sideBar'
 // const allPosts = [...trendingPosts, ...featuredPosts, ...trendingPosts]
 
 function Home() {
+  const { cat, num } = useParams()
+  const { state } = useLocation()
+  const search_query = state && state.searchQuery ? state.searchQuery : null
+
+  const { data, error, loading } = usePostsQuery({
+    pageSize: PAGE_SIZE,
+    page: num ? parseInt(num) - 1 : null,
+    category_id: cat ? parseInt(cat) : null,
+    search_query: search_query,
+    order: 'id asc',
+  })
+
+  if (loading) {
+    return <div>loading</div>
+  }
+  if (error) {
+    return <div> Error : {error.toString()} </div>
+  }
+
+  const postsResponse = data.getPosts
+  // implies err
+  if (postsResponse.msg || postsResponse.type) {
+    return (
+      <div>
+        {postsResponse.type} -- {postsResponse.msg}
+      </div>
+    )
+  }
+
+  const total = postsResponse.total
+  const allPosts = postsResponse.posts.map((post) => {
+    return { ...post, image: '1.jpg' }
+  })
+
   return (
     <main className="home">
       <section className="container">
@@ -25,13 +60,13 @@ function Home() {
           </section>
         </div>
       </section>
-
+      <Divider />
       <section className="container">
         <div className="row">
           <Row>
             <Col lg={17} md={24}>
               <h2 className="section-heading">Recent Posts</h2>
-              {/* <PostsList posts={allPosts} /> */}
+              <PostsList posts={allPosts} page={num} total={total} />
             </Col>
             <Col lg={6} offset={1} md={0}>
               <SideBar />
@@ -39,7 +74,7 @@ function Home() {
           </Row>
         </div>
       </section>
-
+      <Divider />
       <section className="container">
         <div className="row">
           <h2 className="section-heading">Recommended Posts</h2>
